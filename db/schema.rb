3ab4a468c0a20d6cab1cce1ce190c3d17f7b8de0
muaad <muadh24@gmail.com>
2015-09-28 18:05:22 +0300

    Ran some pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928134630) do
+ActiveRecord::Schema.define(version: 20150928144745) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "active",     default: false
+    t.integer  "user_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "accounts", ["user_id"], name: "index_accounts_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",           default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "name"
+    t.string   "profie_pic_file_name"
+    t.string   "profie_pic_content_type"
+    t.integer  "profie_pic_file_size"
+    t.datetime "profie_pic_updated_at"
+    t.string   "phone_number"
+    t.boolean  "is_admin",                default: false
+    t.boolean  "is_support",              default: false
+    t.string   "user_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
