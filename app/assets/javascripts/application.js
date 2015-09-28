d46a8166c532b1a1378feb4d27c3f8b7e6c99ab5
muaad <muadh24@gmail.com>
2015-09-28 17:27:34 +0300

    Added some asset files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,35 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+// require jquery.infinite-pages
+//= require jquery-1.11.1.min
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require_tree .
+// require bootstrap.min
+//= require arjuna
+//= require tasks
+//= require moment.min
+//= require jquery-ui.custom.min
+//= require fullcalendar.min
+//= require jquery.checkboxes
+//= require jquery.datetimepicker
+// require dropzone
+//= require owl-carousel-plugin
+//= require owl-carousel
+//= require jquery.flot.min
+//= require jquery.dataTables
+//= require DT_bootstrap
+//= require jquery.flot.tooltip.min
+//= require jquery.flot.spline
+//= require jquery.flot.pie.min
+//= require jquery.flot.resize
+//= require jquery.flot.grow
+//= require xcharts.min
+//= require d3.v3.min
+//= require cal-heatmap.min
+// require underscore.1.5.2.min
+//= require raphael-min
+//= require morris.min
+//= require morris-script
+// require_tree .
+//= require_self
+
